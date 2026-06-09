e52133a73ac76093bb236adfa944682568cd828c
Hamed-Seddiqi <hamed.seddiqi123@gmail.com>
2026-06-09 15:17:04 +0430

    some JavaScript is added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-document.getElementById('Hamed').innerHTML = 'Welcome night'
+// document.getElementById('Hamed').innerHTML = 'Welcome night'
+// document.write('welcome Ahmad');
+// window.alert("welcome Back Moahmmad")
+// console.log('Ahmad Jan');
+// console.log('Why you come let ahmad today is your last day you come let in the clas')
+// let x = 20;
+// let y = 200;
+// z = x+y;
+// document.getElementById('Hamed2').innerHTML =  z;
+
+
